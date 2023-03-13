f81288b2b0f7b2cd800ffaca05b22ec9713b7fe3
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-13 13:53:52 +0300

    HW_Task 1_1

---
+++ Lesson_1/HW_lesson_1/1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и на выходе выдает, какое большее, какое меньшее.
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b)
+{
+  Console.WriteLine("Первое число " + a + " больше второго " + b);
+}
+else
+{
+  Console.WriteLine("Второе число " + b + " больше первого "  + a);
+}
