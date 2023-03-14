9ec4bee92490c3bbedbbc78f8b28b8832d6d7873
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-14 16:26:12 +0300

    Task_2_0

---
+++ Lesson_1/Lesson_2/2_0/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает 
+// трехзначное число и на выходе показывает последнюю цифру этого числа.
+
+// 123 / 10 -> 12 % 10 -> 2
+// 123 % 10 -> 3
+
+int TakeNum(int num)
+{
+  Console.WriteLine(num);
+  return num % 10;
+}
+
+int th_num = new Random().Next(100, 1000);
+int result = TakeNum(th_num);
+Console.WriteLine(result);
