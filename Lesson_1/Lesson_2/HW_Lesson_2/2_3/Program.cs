6588b97937a6409ca6e61ed13569a29fd7ba3867
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-15 20:54:12 +0300

    hw_task_2_3

---
+++ Lesson_1/Lesson_2/HW_Lesson_2/2_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void Function(int num)
+{
+  string text = "Это не день недели.";
+  if (num ==7 || num == 6)
+  {
+    text = "Выходные.";
+  }
+  else if (num<6 && num>0)
+  {
+    text = "Рабочийе дни";
+  }
+  Console.WriteLine(num + " - " + text);
+}
+
+int val = int.Parse(Console.ReadLine()!);
+Function(val);
