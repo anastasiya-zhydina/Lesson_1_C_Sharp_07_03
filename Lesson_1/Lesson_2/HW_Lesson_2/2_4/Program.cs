a3090c1c24b04c0fe5dbdf152d4c87968ad26804
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-15 21:27:30 +0300

    hw_task_2_4

---
+++ Lesson_1/Lesson_2/HW_Lesson_2/2_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное 
+//трехзначное число и удаляет вторую цифру этого числа.
+
+string FunctionNum(int num)
+{
+  if ((-1000<num && num>-99) || (num<1000 && num>99))
+  {
+    return $"{num} - {num/100*10 + num%10}";
+  }
+  return "Данное число не является трехзначным.";
+}
+
+int val = int.Parse(Console.ReadLine()!);
+string result = FunctionNum(val);
+Console.WriteLine(result);
