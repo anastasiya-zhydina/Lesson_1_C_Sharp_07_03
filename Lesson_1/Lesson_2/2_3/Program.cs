ee06d81b5cff646959a2e0cf246561903e9bc3d8
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-15 16:53:53 +0300

    Task_2_3

---
+++ Lesson_1/Lesson_2/2_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет
+// кратно ли оно одновременно 7 и 23.
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+void MyFunction(int num)
+{
+  if (num%7 == 0 && num%23 == 0)
+  {
+    Console.WriteLine("Число " + a + " кратно 7 и 23 одновременно.");
+  }
+  else
+  {
+    Console.Write("Число " + a + " не кратно 7 и 23 одновременно.");
+  }
+}
+
+MyFunction(a);
