8f2fde06ffdd06214130e1e8f79b988740ba1c11
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-15 17:57:19 +0300

    hw_task_2_1

---
+++ Lesson_1/Lesson_2/HW_Lesson_2/2_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+int FerstFunction(int n)
+{
+  return n/10;
+}
+
+int SecondFunction(int n)
+{
+  return n%10;
+}
+
+void PrintNumber(int n)
+{
+  Console.WriteLine("Значение второго числа в трехзначном: " + n);
+}
+
+int num1 = FerstFunction(a);
+int num2 = SecondFunction(num1);
+PrintNumber(num2);
