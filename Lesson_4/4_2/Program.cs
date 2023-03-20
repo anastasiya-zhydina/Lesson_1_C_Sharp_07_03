73b53d7dcd485cbfabf6ed9ac1f8ba9d13c4d23e
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-20 20:08:05 +0300

    task_4_2

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает
+// произведение чисел от 1 до N.
+
+int num = GetUserNumber();
+int digit = MultiplyNumbers(num);
+Console.WriteLine("Факториал введенного числа: " + digit);
+
+int GetUserNumber()
+{
+  int number = 0;
+  while (number == 0)
+  {
+    Console.Write("Пожалуйста, введите число: ");
+    number = int.Parse(Console.ReadLine()!);
+  }
+  return number;
+}
+
+int MultiplyNumbers(int n)
+{
+  int multi = 1;
+  for(int i = 1; i <= n; i++)
+  {
+    multi = multi * i;
+  }
+  return multi;
+}
