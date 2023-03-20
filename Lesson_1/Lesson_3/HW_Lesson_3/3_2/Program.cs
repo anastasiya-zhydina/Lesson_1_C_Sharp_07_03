b0aff84b58c8b8222a16f54d2cda55090b2ec90a
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-20 16:53:52 +0300

    hw_test_3_2

---
+++ Lesson_1/Lesson_3/HW_Lesson_3/3_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите целое положитльное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+void MyFunction(int a)
+{
+  if (a<=0)
+  {
+    Console.WriteLine("Введите ПОЛОЖИТЕЛЬНОЕ число, отличное от 0!");
+  }
+  else
+  {
+    for (int i = 1; i <= a; i++)
+  {
+    Console.WriteLine(Math.Pow(i,3));
+  }
+  }
+}
+
+Console.WriteLine();
+MyFunction(num);
