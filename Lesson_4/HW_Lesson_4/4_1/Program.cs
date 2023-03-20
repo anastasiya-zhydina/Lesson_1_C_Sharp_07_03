2eef7c8b3ebe0e3fede2a9416f6306558f84f3a9
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-20 23:09:05 +0300

    hw_task_4_1

---
+++ Lesson_4/HW_Lesson_4/4_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int num = Fuction1(a, b);
+Console.WriteLine($"Число {a} в степени {b} равно - " + num);
+
+int Fuction1(int a, int b)
+{
+  int c = 1;
+  for(int i = 1; i <= b; i++)
+  {
+    c = c*a;
+  }
+  return c;
+}
+
