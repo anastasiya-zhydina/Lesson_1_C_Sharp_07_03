2d609ea62f47947a4b8e62e3b0390b5a072c79a8
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-20 16:11:58 +0300

    test_3_3

---
+++ Lesson_1/Lesson_3/3_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу
+// квадратов чисел от 1 до N.
+
+void Pow(int num)
+{
+  int i = 1;
+  while (i <= num)
+  {
+    Console.WriteLine(Math.Pow(i,2));
+    i++;
+  }
+}
+
+int number = int.Parse(Console.ReadLine()!);
+Pow(number);
