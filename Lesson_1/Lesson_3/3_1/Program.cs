33bd997cdd0c586b666fa279283696213fbccd73
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-20 15:37:55 +0300

    task_3_1

---
+++ Lesson_1/Lesson_3/3_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x, y).
+
+void GetQuarter()
+{
+  Console.Write("Пожалуйста, введите номер четверти: ");
+  int q = int.Parse(Console.ReadLine()!);
+  if (q < 1 || q > 4)
+  {
+    Console.WriteLine("Такой четверти нет.");
+  }
+  else
+  {
+    switch (q)
+    {
+      case 1:
+        Console.WriteLine("x > 0, y > 0");
+        break;
+      case 2:
+        Console.WriteLine("x < 0, y > 0");
+        break;
+      case 3:
+        Console.WriteLine("x < 0, y < 0");
+        break;
+      case 4:
+        Console.WriteLine("x > 0, y < 0");
+        break;
+      default:
+        Console.WriteLine("Такой четверти нет.");
+        break;
+    }
+  }
+}
+
+GetQuarter();
