94920856a7f9e069c7f79e5d0b096aa2dd9bf3b9
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-21 15:35:04 +0300

    hw_task_4_3

---
+++ Lesson_4/HW_Lesson_4/4_3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = MakeArray();
+PrintArray(array);
+
+int GenerateNumbers()
+{
+  Random rnd = new Random();
+  int value =rnd.Next();
+  return value;
+}
+
+int[] MakeArray()
+{
+  int[] arr = new int[8];
+  for(int i = 0; i < 8; i++)
+  {
+    arr[i]=  GenerateNumbers();
+  }
+  return arr;
+}
+
+void PrintArray(int[] array)
+{
+  Console.Write("[ ");
+  for(int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.Write("]");
+}
