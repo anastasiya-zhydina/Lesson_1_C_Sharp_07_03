92dff66b9b83a7fd40e61fdf6a070452b7701357
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-26 19:01:10 +0300

    task_6_0

--- Lesson_4/Lesson_6/6_0/Program.cs
+++ Lesson_4/Lesson_6/6_0/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая перевернет одномерный массив
+// последний элемент будет на первом месте, а первый на последнем.
+
+void Print(int[] arr)
+{
+  int size = arr.Length;
+
+  for (int i = 0; i < size; i++)
+    Console.Write($"{arr[i]} ");
+  Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+  int[] arr = new int[size];
+
+  for (int i = 0; i < size; i++)
+    arr[i] = new Random().Next(from, to + 1);
+
+  return arr;
+}
+
+void RevMass(int[] arr)
+{
+  int size = arr.Length;
+  for (int i = 0; i < size / 2; i++)
+    (arr[i], arr[size - i - 1]) = (arr[size - i - 1], arr[i]); // способ кортежей
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+RevMass(mass);
+Print(mass);
