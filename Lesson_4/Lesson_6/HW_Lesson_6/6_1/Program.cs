f546efe4977fd0a5eb94d6613918cbf26a49b427
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-03-31 15:08:13 +0300

    hw_task_6_1

---
+++ Lesson_4/Lesson_6/HW_Lesson_6/6_1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+
+Console.Write("Введите элементы через пробел: ");
+
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+
+Console.WriteLine($"Количество элементов больше нуля: {count}.");
+
+//------
+
+//string line = Console.ReadLine();
+//string[] splitLine = line.Split(' ');
