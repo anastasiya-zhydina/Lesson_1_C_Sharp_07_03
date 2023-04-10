45e098d4bfbc487b04dfff68cc33bfbc19bf3b00
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-04-10 18:28:57 +0300

    task_9_2

---
+++ Lesson_4/Lesson_9/9_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет на вход принимать число и возвращать сумму его цифр.
+// Использовать рекурсию.
+
+int SumNumbersCount(int a)
+{
+  if(a < 10) return a;
+  return SumNumbersCount(a/10) + a%10;
+}
+Console.WriteLine("==================");
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр числа {a} равна -> {SumNumbersCount(a)}.");
+Console.WriteLine("==================");
