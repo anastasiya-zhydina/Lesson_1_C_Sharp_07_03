834128b57c43c094d650f9d914152ee330931ac0
Anastasiya Zhydina <zhydina.nasty@yandex.ru>
2023-04-10 17:52:32 +0300

    task_9_0

---
+++ Lesson_4/Lesson_9/9_0/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от 1 до N при помощи рекурсии.
+
+void PrintNumbers(int n)
+{
+  if(n==0) return;
+  PrintNumbers(n-1);
+  Console.Write($"{n} ");
+}
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Результат: ");
+PrintNumbers(n);
